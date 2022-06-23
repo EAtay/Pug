00bc8f0bf4587c84e3aea54c313bdaf78690b7a3
EA <atai.estebesov@gmail.com>
2022-06-23 11:56:14 +0600

    fixed adaptation on main page

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,73 @@
-const path = require('path') 
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
-const CopyWebpackPlugin = require('copy-webpack-plugin') 
- 
-module.exports = { 
-  mode: 'development', 
-  entry: [ 
-    './src/index.pug', 
-    './src/index.js', 
-    './src/index.scss', 
-  ], 
-  output: { 
-    path: path.resolve(__dirname, 'dist'), 
-    filename: '[name].js', 
-    clean: true, 
-  }, 
-  devtool: 'source-map', 
-  devServer: { 
-    static: { 
-      directory: path.resolve(__dirname, 'dist'), 
-    }, 
-    port: 3000, 
-    open: true, 
-    hot: true, 
-    compress: true, 
-  }, 
-  module: { 
-    rules: [ 
-      { 
-        test: /.pug$/, 
-        use: ['pug-loader'], 
-      }, 
-      { 
-        test: /\.scss$/, 
-        use: ['style-loader', 'css-loader', 'sass-loader'], 
-      }, 
-      { 
-        test: /\.(png|svg|jpg|jpeg|gif|ttf)$/i, 
-        type: 'asset/resource', 
-      }, 
-    ], 
-  }, 
-  plugins: [ 
-    new HtmlWebpackPlugin({ 
-      title: 'Webpack App', 
-      filename: 'index.html', 
-      template: 'src/index.pug', 
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ttf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/components/main.pug",
     }),
-    new CopyWebpackPlugin({ 
-      patterns: [ 
-        { 
-          from: './src/img', 
-          to: './img', 
-        }, 
-      ], 
+
+    new HTMLWebpackPlugin({
+      filename: "trening.html",
+      template: "./src/pages/trening.pug",
     }),
-  ], 
-}
+    new HTMLWebpackPlugin({
+      filename: "service.html",
+      template: "./src/pages/service.pug",
+    }),
+
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/img",
+          to: "./img",
+        },
+      ],
+    }),
+  ],
+};
